f340c527a889aeb2009974ba3a850e476e35708a
Fati CHEN <chen@lirmm.fr>
2019-01-24 17:14:02 +0100

    updating dist delivery

---
+++ build/index.d.ts
@@ -0,0 +1,25 @@
+/**
+ * Implementation of RWordle-L Algorithm
+ *
+ * Ported from Java, original code owned by :
+ * Copyright by Marc Spicker
+ * GPL License -- http://www.gnu.org/licenses/gpl.html
+ *
+ * based on the publication:
+ * @article {CGF:CGF3106,
+ *  author = {Strobelt, H. and Spicker, M. and Stoffel, A. and Keim, D. and Deussen, O.},
+ *  title = {Rolled-out Wordles: A Heuristic Method for Overlap Removal of 2D Data Representatives},
+ *  journal = {Computer Graphics Forum},
+ *  volume = {31},
+ *  number = {3pt3},
+ *  publisher = {Blackwell Publishing Ltd},
+ *  issn = {1467-8659},
+ *  url = {http://dx.doi.org/10.1111/j.1467-8659.2012.03106.x},
+ *  doi = {10.1111/j.1467-8659.2012.03106.x},
+ *  pages = {1135--1144},
+ *  keywords = {I.3.3 [Computer Graphics]: Picture/Image Generation—Line and curve generation},
+ *  year = {2012},
+ * }
+ */
+import Algorithm from "agora-algorithm";
+export declare const rWordle: Algorithm.Function;
